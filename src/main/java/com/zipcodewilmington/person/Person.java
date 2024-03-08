83b585daf1612b711c83ca00bb6488ab417129af
nathaneckel <nathan@patientpaperwork.com>
2024-03-07 20:16:07 -0500

    big progress

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -7,29 +7,83 @@ public class Person {
     private String name;
     private int age;
 
+    //ha
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
-    public Person(String name, int age) {
+    //added variables gender/generation/hobby/netWorth/isMarried
+    public Person(String name, int age, String gender, String generation, String hobby, int netWorth, Boolean isMarried) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.generation = generation;
+        this.hobby = hobby;
+        this.netWorth = netWorth;
+        this.isMarried = isMarried;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setGeneration(String generation) {
+        this.generation = generation;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public void setNetWorth(Integer netWorth) {
+        this.netWorth = netWorth;
+    }
+
+    public void setIsMarried(Boolean isMarried) {
+        this.isMarried = isMarried;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
+    }
+    public String getGender() {
+        return this.gender;
+    }
+
+    public String getGeneration() {
+        return this.generation;
+    }
+
+    public String getHobby() {
+        return this.hobby;
+    }
+
+    public Integer getNetWorth() {
+        return this.netWorth;
+    }
+
+    public Boolean getIsMarried() {
+        return this.isMarried;
     }
 }
